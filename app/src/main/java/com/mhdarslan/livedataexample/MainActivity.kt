4adc02bf80987fbf2dac992558c885df41e3c2dc
Muhammad Arslan <gery_maz_555@hotmail.com>
2026-05-03 20:53:36 +0500

    MutableLiveData updated

--- app/src/main/java/com/mhdarslan/livedataexample/MainActivity.kt
+++ app/src/main/java/com/mhdarslan/livedataexample/MainActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.mhdarslan.livedataexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        mainViewModel.factsLiveData.observe(this, Observer{
+            binding.txtData.text = it
+        })
+
+        binding.btnUpdate.setOnClickListener {
+            mainViewModel.updateLiveData()
         }
+
+
     }
 }
